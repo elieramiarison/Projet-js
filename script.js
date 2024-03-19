f72aeb29665effab2d32b1285fc96458aa6cac78
elieramiarison <eliespirale1@gmail.com>
2024-03-19 03:07:58 +0300

    Ajout des trois fichiers

---
+++ script.js
@@ -0,0 +1,28 @@
+let inputAdd = document.getElementById("nouvTache");
+let add = document.getElementById("add");
+let clear = document.getElementById("clear");
+let somme = 0;
+
+add.addEventListener("click", () => {
+    let inputA = inputAdd.value;
+    somme += somme
+
+    const btn = document.createElement("button");
+    btn.innerText = "Remove";
+    const valeur = document.createElement("h2");
+    valeur.innerText = inputA;
+    const semble = document.querySelector(".semble");
+    semble.appendChild(valeur);
+    semble.appendChild(btn);
+    inputAdd.value = "";
+
+    btn.addEventListener('click', () => {
+        semble.removeChild(valeur);
+        semble.removeChild(btn);
+    })
+})
+
+clear.addEventListener("click", () => {
+    const semble = document.querySelector(".semble");
+    semble.innerHTML = '';
+})
